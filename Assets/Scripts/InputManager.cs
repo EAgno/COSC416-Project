0869380b7b18528f5bad0c5e0677e9ee7bdb5045
EAgno <eagno@student.ubc.ca>
2025-03-18 08:45:02 -0700

    Making an initial scene to test 2d

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
-
-public class IInputManager : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static InputManager Instance;
+
+    private Vector2 movementInput;
+    public bool jumpPressed;
+    public bool attackPressed;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Capture movement input (WASD or Arrow keys)
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        // Capture jump input (Spacebar or Joystick button 0)
+        jumpPressed = Input.GetButtonDown("Jump");
+
+        // Capture attack input (Left Mouse Button or Joystick button 1)
+        attackPressed = Input.GetButtonDown("Fire1");
+    }
+
+    public Vector2 GetMovementInput()
+    {
+        return movementInput.normalized;
     }
 }
